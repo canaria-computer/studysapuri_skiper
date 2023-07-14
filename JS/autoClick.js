435e073fe179d5d67e05039b60efd41db0ae7d39
canaria-computer <121685177+canaria-computer@users.noreply.github.com>
2023-07-15 00:15:19 +0900

    add function

--- JS/autoClick.js
+++ JS/autoClick.js
@@ -1,4 +1,4 @@
-var shuffleArray = (array) => {
+function shuffleArray(array) {
     const cloneArray = [...array];
     for (let i = cloneArray.length - 1; i >= 0; i--) {
         let rand = Math.floor(Math.random() * (i + 1));
@@ -7,13 +7,42 @@ var shuffleArray = (array) => {
         cloneArray[rand] = tmpStorage;
     }
     return cloneArray;
-};
+}
 
-setInterval(() => {
-    shuffleArray(document.querySelectorAll("[class*=TopicsPage__Main]  button"))
-        .forEach(each => each.click());
-})
+function allRandomClick() {
+    shuffleArray(document.querySelectorAll("[class*=TopicsPage__Main]  button")).forEach(each => each.click());
+}
 
-setInterval(() => {
+function sendKeyClick() {
     document.querySelector('button[class*=RaisedButton]').click();
-}, Math.random() * (10000 - 1000) + 1000)
+}
+
+var click = setInterval(allRandomClick);
+var send = setInterval(sendKeyClick, Math.random() * (10000 - 1000) + 1000);
+
+function kill() {
+    clearInterval(click);
+    clearInterval(send);
+}
+
+function reStart() {
+    return [setInterval(allRandomClick), setInterval(sendKeyClick, Math.random() * (10000 - 1000) + 1000)]
+}
+
+let count = 0;
+document.addEventListener("keydown", (event) => {
+    count++;
+
+    if (event.code === "F8") {
+        if (count % 2) {
+            kill();
+        } else {
+            [click, send] = reStart();
+        }
+    }
+    if (event.code === "F9") {
+        kill();
+        console.warn("Auto Click stop.");
+        console.warn("To resume, call `reStat()` manually.");
+    }
+});
